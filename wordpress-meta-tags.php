3c9fadd9bd99363d42ff3e6a2ce475f681bb3b17
Mohammad Javad Karimi <103068025+mj-Karimi@users.noreply.github.com>
2023-12-13 16:38:06 +0330

    Update wordpress-meta-tags.php

--- wordpress-meta-tags.php
+++ wordpress-meta-tags.php
@@ -5,12 +5,14 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="language" content="en">
 		<meta name="geo.region" content="US">
-		<meta name="copyright" content="copyright (c) by ">
+		<meta name="copyright" content="copyright (c) by">
 		<meta name="author" content="mj">
+	
 		<meta data-react-helmet="true" name="msapplication-TileImage" content="https://domain.com"/>
 		<meta data-n-head="1" data-hid="mobile-web-app-capable" name="mobile-web-app-capable" content="yes">
 		<meta data-n-head="1" data-hid="apple-mobile-web-app-title" name="apple-mobile-web-app-title" content="<?php the_title() ?>">
 		<meta name="keywords" content="<?php the_title() ?>">
+		<meta name="description" content="<?php the_title() ?>">
 		<link rel="icon" type="image/png" href="https://domain.com/favicon.ico">
 		<meta name="msapplication-TileColor" content="#24a355">
 		<meta name="msapplication-navbutton-color" content="#24a315">
